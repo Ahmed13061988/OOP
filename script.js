42dec689ab5c709e137b09137ed4e40e36137394
Ahmed Hussein <wolktheline@yahoo.com>
2023-02-23 19:29:11 -0700

    how construction function works

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 
 const Person = function (firstName, birthYear) {};
 
-const ahmed = new Person('ahmed', 1991);
+new Person('ahmed', 1988);
+
+//1. New empty object is created
+//2. Function is called, this keyword will be the new object
+//3.new object is linked to prototype
+//4.The object is auto returned from construction function
