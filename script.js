95ddf94c615c2478fc89eccc5f510c99e132df52
Ahmed Hussein <wolktheline@yahoo.com>
2023-02-23 19:34:03 -0700

    adding properties to the function using this keyword

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
 
-new Person('ahmed', 1988);
-
+const ahmed = new Person('ahmed', 1988);
+console.log(ahmed);
 //1. New empty object is created
 //2. Function is called, this keyword will be the new object
 //3.new object is linked to prototype
