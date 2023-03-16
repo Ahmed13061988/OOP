2c1214f26de2be23dfe2521a5f3f6ed714fdec8e
Ahmed Hussein <wolktheline@yahoo.com>
2023-03-15 20:31:36 -0600

    prototype

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
@@ -17,7 +17,17 @@ console.log(ahmed);
 console.log(matilda);
 console.log(john);
 console.log(ahmed instanceof Person);
-//1. New empty object is created
-//2. Function is called, this keyword will be the new object
-//3.new object is linked to prototype
-//4.The object is auto returned from construction function
+// //1. New empty object is created
+// //2. Function is called, this keyword will be the new object
+// //3.new object is linked to prototype
+// //4.The object is auto returned from construction function
+
+//Prototypes
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+console.log(Person.prototype);
+
+ahmed.calcAge();
